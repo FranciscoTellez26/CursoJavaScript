3d82a5175f11fb1c389701a05cd7f4e8b1b4d567
FranciscoTellez26 <francisco.juan.tellez93@gmail.com>
2023-11-01 15:40:37 -0600

    operadores en JavaScript

--- Leccion03/03-01OperadoresJavaScript.js
+++ Leccion03/03-01OperadoresJavaScript.js
@@ -1,3 +1,4 @@
+/*
 let a = 3, b = 2;
 
 let  z = a + b;
@@ -23,7 +24,7 @@ console.log(z);
 z  = b--;
 console.log(b);
 console.log(z);
-
+*/
 
 /* 
 console.log("El resultado de la suma es: " + z);
@@ -44,3 +45,51 @@ z = a ** b; //Exponente
 console.log("El exponente es: " + z)
 
 */
+
+/*
+//Precedencia de operadores
+let a = 3, b = 2, c = 1, d = 4;
+
+let z = a * b + c / d; //Se ejecuta primeto la multiplicaci'on, despues la division y al final la suma
+console.log(z)
+
+z =  c + a * b / d; //Se ejecuta primeto a * b, el resultado de esto se multiplica ṕor d y al final se suma c
+console.log(z)
+
+z = (c + a) * b / c; //Se puede alterar el orden que se eejecutan las operaciones utilizando parentesis
+console.log (z);
+*/
+
+/*
+//Operador de asgnación (los valores se asignan de izquierda a derecha).
+let a = 1;
+
+a += 3;  //Esto es igual a hacer a = a + 3 (Asignación compuesto)
+console.log(a);
+
+
+ a-= 2; //Esto es igual a hacer a = a -2 (Asignación compuesto)
+ console.log(a);
+ 
+ *=
+ /=
+ %=
+ **=
+ */
+
+/*
+ //Operadores de comparación
+let a = 3, b = 2, c = "3";
+
+let z = a == b; // Se revisa el valor sin importar el tipo
+console.log(z)
+
+z = a === c; //Revisa los valores, per tambien los tipos
+console.log(z)
+*/
+
+//operador ternario 
+
+let resultado = (3>2) ? "verdadero" : "falso";
+
+console.log(resultado);
